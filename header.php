4db7c5955c16af4c3a0e2a27278650b1429ef1a8
xAm64 <48089283+xAm64@users.noreply.github.com>
2020-08-25 14:20:16 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<title>Mon titre</title>
+	<meta name="viewport" contant="width=device-width, initial-scale=1">
+	<meta name="Author" content="Manex">
+	<?php wp_head() ?>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+<h1> page de test</h1>
+<p>Mon contenue </p>
+<?php get_footer(); ?>
